d0654c3c1b078d9a0cd93bf1ef8909247a359238
Johann Schopplich <mail@johannschopplich.com>
2021-09-20 13:17:47 +0200

    refactor: rename log level type

--- src/log.ts
+++ src/log.ts
@@ -2,15 +2,15 @@ import colors from 'chalk'
 import { name } from '../package.json'
 import type { Color } from 'chalk'
 
-type MessageType = 'info' | 'success' | 'error'
+type LogLevel = 'info' | 'success' | 'error'
 
-const colorMap = new Map<MessageType, typeof Color>([
+const colorMap = new Map<LogLevel, typeof Color>([
   ['info', 'yellow'],
   ['success', 'green'],
   ['error', 'red']
 ])
 
-export function log(message: string, type: MessageType = 'info') {
+export function log(message: string, type: LogLevel = 'info') {
   const content = [
     colors.gray(`[${name}]`),
     colors[colorMap.get(type) ?? 'white'](message)
