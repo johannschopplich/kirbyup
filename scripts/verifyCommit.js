da3f7f6f82a9840e3b7955538fb62c4fe540dfed
Johann Schopplich <mail@johannschopplich.com>
2021-09-23 16:48:03 +0200

    chore: lint commit messages

---
+++ scripts/verifyCommit.js
@@ -0,0 +1,30 @@
+// @ts-check
+
+/*
+ * Modified from https://github.com/vitejs/vite/blob/main/scripts/verifyCommit.js
+ */
+const fs = require('fs')
+const colors = require('nanocolors')
+const msgPath = process.argv[2]
+const msg = fs.readFileSync(msgPath, 'utf-8').trim()
+
+const RELEASE_RE = /^v\d/
+const COMMIT_RE =
+  /^(revert: )?(feat|fix|docs|dx|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps)(\(.+\))?: .{1,50}/
+
+if (!RELEASE_RE.test(msg) && !COMMIT_RE.test(msg)) {
+  console.log()
+  console.error(
+    `  ${colors.bgRed(colors.white(' ERROR '))} ${colors.red(
+      `invalid commit message format.`
+    )}\n\n` +
+      colors.red(
+        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
+      ) +
+      `    ${colors.green(`feat: add 'comments' option`)}\n` +
+      `    ${colors.green(`fix: handle events on blur (close #28)`)}\n\n` +
+      colors.red(`  See .github/commit-convention.md for more details.\n`)
+  )
+
+  process.exit(1)
+}
