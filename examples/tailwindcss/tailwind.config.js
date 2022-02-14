891648307d7631a77f7311841ad806e32f07ed45
Johann Schopplich <mail@johannschopplich.com>
2022-02-14 22:45:03 +0100

    chore: upgrade deps of example setups

--- examples/tailwindcss/tailwind.config.js
+++ examples/tailwindcss/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig} */
 module.exports = {
-  mode: 'jit',
-  purge: ['src/**/*.{vue,js,ts}'],
+  content: {
+    files: ['src/**/*.{vue,js,ts}']
+  },
   darkMode: false,
   theme: {
     extend: {}
