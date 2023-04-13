096a10d8c2d6ec7af847590c6739765a362afcd8
Johann Schopplich <mail@johannschopplich.com>
2023-04-13 09:32:09 +0200

    chore: update comment verification regex

--- scripts/verifyCommit.mjs
+++ scripts/verifyCommit.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 // Invoked on the `commit-msg` git hook by simple-git-hooks
-// Modified from https://github.com/vitejs/vite/blob/main/scripts/verifyCommit.ts
 
 import { readFileSync } from 'node:fs'
 import colors from 'picocolors'
@@ -10,7 +9,7 @@ const msgPath = process.argv[2]
 const msg = readFileSync(msgPath, 'utf-8').trim()
 
 const RELEASE_RE = /^v\d/
-const COMMIT_RE = /^(revert: )?(feat|fix|docs|dx|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps)(\(.+\))?: .{1,50}/
+const COMMIT_RE = /^(revert: )?(feat|fix|docs|style|refactor|perf|test|build|ci|chore)(\(.+\))?!?: .{1,50}/
 
 if (!RELEASE_RE.test(msg) && !COMMIT_RE.test(msg)) {
   console.log()
@@ -21,10 +20,9 @@ if (!RELEASE_RE.test(msg) && !COMMIT_RE.test(msg)) {
       colors.red(
         '  Proper commit message format is required for automated changelog generation. Examples:\n\n',
       )
-      }    ${colors.green('feat: add \'comments\' option')}\n`
+      }    ${colors.green('feat: add `comments` option')}\n`
       + `    ${colors.green('fix: handle events on blur (close #28)')}\n\n${
       colors.red('  See .github/commit-convention.md for more details.\n')}`,
   )
-
   process.exit(1)
 }
