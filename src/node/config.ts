1b321e9c9f1617786d1f200b6c96b6af44bd2a2b
Johann Schopplich <mail@johannschopplich.com>
2023-12-09 11:35:32 +0100

    chore: remove unused imports

--- src/node/config.ts
+++ src/node/config.ts
@@ -1,8 +1,5 @@
 import { loadConfig as _loadConfig } from 'c12'
-import type { LoadConfigResult, LoadConfigSource } from 'unconfig'
 import type { UserConfig } from './types'
-
-export type { LoadConfigResult, LoadConfigSource }
 
 export function loadConfig(cwd = process.cwd()) {
   return _loadConfig<UserConfig>({
