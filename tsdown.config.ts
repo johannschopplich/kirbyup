8fc70e94ef6d8067c4497d9a413dbb917523d837
Johann Schopplich <mail@johannschopplich.com>
2025-09-22 11:29:53 +0200

    perf: optimize tsdown build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,17 +2,16 @@ import type { UserConfig, UserConfigFn } from 'tsdown/config'
 import { defineConfig } from 'tsdown/config'
 
 const config: UserConfig | UserConfigFn = defineConfig({
-  entry: [
-    'src/node/index.ts',
-    'src/node/cli.ts',
-    'src/client/config.ts',
-    'src/client/plugin.ts',
-  ],
+  entry: {
+    'node/index': 'src/node/index.ts',
+    'node/cli': 'src/node/cli.ts',
+    'client/config': 'src/client/config.ts',
+    'client/plugin': 'src/client/plugin.ts',
+  },
   external: ['rollup'],
   define: {
     'process.env.VITEST': 'undefined',
   },
-  unbundle: true,
 })
 
 export default config
