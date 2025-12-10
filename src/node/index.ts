4fbc6a561b2b8efa1740a525244a32a2f6c32309
Johann Schopplich <mail@johannschopplich.com>
2025-12-10 12:34:53 +0100

    feat: `.vite-running` marker plugin

--- src/node/index.ts
+++ src/node/index.ts
@@ -15,8 +15,7 @@ import * as vueCompilerSfc from 'vue/compiler-sfc'
 import { name, version } from '../../package.json'
 import { loadConfig, resolvePostCSSConfig } from './config'
 import { handleError, PrettyError } from './errors'
-import kirbyupBuildCleanupPlugin from './plugins/build-cleanup'
-import kirbyupHmrPlugin from './plugins/hmr'
+import { kirbyupBuildCleanupPlugin, kirbyupHmrPlugin, kirbyupRunningMarkerPlugin } from './plugins'
 import { printFileInfo, toArray } from './utils'
 import { resolveOriginFromServerOptions } from './utils/server'
 
@@ -83,6 +82,7 @@ function getViteConfig(
     const serveConfig: InlineConfig = mergeConfig(sharedConfig, {
       plugins: [
         kirbyupHmrPlugin(options as ServeOptions),
+        kirbyupRunningMarkerPlugin({ outDir: options.outDir }),
         watch && fullReloadPlugin(watch),
       ].filter(Boolean),
       // Input needs to be specified so dependency pre-bundling works
@@ -106,7 +106,10 @@ function getViteConfig(
 
   const buildConfig: InlineConfig = mergeConfig(sharedConfig, {
     mode,
-    plugins: [kirbyupBuildCleanupPlugin(options as BuildOptions)],
+    plugins: [
+      kirbyupBuildCleanupPlugin(options as BuildOptions),
+      ...(options.watch ? [kirbyupRunningMarkerPlugin({ outDir: options.outDir })] : []),
+    ],
     build: {
       lib: {
         entry: resolve(options.cwd, options.entry),
