cc53b90b29cdd578453f432f118fa90e8307bcce
Johann Schopplich <mail@johannschopplich.com>
2025-12-10 11:37:33 +0100

    chore: default to Vue 3 compat mode

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,7 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  vue: {
-    // https://github.com/antfu/eslint-config/issues/367
-    sfcBlocks: {
-      blocks: {
-        styles: false,
-      },
-    },
-    vueVersion: 2,
-  },
+  pnpm: false,
   ignores: ['examples/**/*.vue'],
 }).append({
   rules: {
