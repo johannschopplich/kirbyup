b20be5bcf19ff26938ddfdefc497ce46dee47d56
Johann Schopplich <mail@johannschopplich.com>
2026-01-05 09:48:49 +0100

    chore: add new social card image

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,6 +7,7 @@ import {
   ogImage,
   ogUrl,
   releases,
+  twitterImage,
 } from './meta'
 
 export default defineConfig({
@@ -23,7 +24,7 @@ export default defineConfig({
     ['meta', { property: 'og:image', content: ogImage }],
     ['meta', { name: 'twitter:title', content: name }],
     ['meta', { name: 'twitter:description', content: description }],
-    ['meta', { name: 'twitter:image', content: ogImage }],
+    ['meta', { name: 'twitter:image', content: twitterImage }],
     ['meta', { name: 'twitter:site', content: '@jschopplich' }],
     ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
   ],
--- docs/.vitepress/meta.ts
+++ docs/.vitepress/meta.ts
@@ -1,6 +1,7 @@
 /* VitePress head */
 export const ogUrl = 'https://kirbyup.getkirby.com/'
 export const ogImage = `${ogUrl}og.png`
+export const twitterImage = `${ogUrl}twitter.png`
 
 /* GitHub and social links */
 export const github = 'https://github.com/johannschopplich/kirbyup'
