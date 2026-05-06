cd1eabbce5adbeba121530aa84e147e7a1e72427
Johann Schopplich <johann@schopplich.com>
2026-05-06 21:35:03 +0200

    refactor: simplify HMR reload function and remove unused test case

--- src/node/plugins/runtime/hmr-shim.js
+++ src/node/plugins/runtime/hmr-shim.js
@@ -5,19 +5,10 @@ if (typeof __VUE_HMR_RUNTIME__ !== 'undefined') {
   const originalReload = __VUE_HMR_RUNTIME__.reload
 
   __VUE_HMR_RUNTIME__.reload = function (id, newComp) {
-    const plugins = window.panel && window.panel.plugins
-    const app = window.panel && window.panel.app
+    const { plugins, app } = window.panel ?? {}
 
-    if (
-      plugins
-      && app
-      && plugins.components
-      && typeof plugins.resolveComponentExtension === 'function'
-      && typeof plugins.resolveComponentRender === 'function'
-      && typeof plugins.resolveComponentMixins === 'function'
-    ) {
-      for (const name in plugins.components) {
-        const pluginComp = plugins.components[name]
+    if (plugins?.components) {
+      for (const [name, pluginComp] of Object.entries(plugins.components)) {
         if (
           pluginComp.__hmrId === id
           || (newComp.__file && pluginComp.__file === newComp.__file)
@@ -30,7 +21,7 @@ if (typeof __VUE_HMR_RUNTIME__ !== 'undefined') {
       }
     }
 
-    return originalReload.call(this, id, newComp)
+    return originalReload(id, newComp)
   }
 }
 else {
