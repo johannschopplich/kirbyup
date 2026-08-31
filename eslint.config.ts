9b67e20bbee5afb69001daf14227d649e62a5025
Johann Schopplich <johann@schopplich.com>
2026-08-31 16:28:09 +0200

    chore: upgrade dependencies

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,7 +3,6 @@ import type { FlatConfigComposer } from 'eslint-flat-config-utils'
 import antfu from '@antfu/eslint-config'
 
 const config: FlatConfigComposer<TypedFlatConfigItem, ConfigNames> = antfu({
-  pnpm: false,
   ignores: ['examples/**/*'],
 }).append({
   rules: {
